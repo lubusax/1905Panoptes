02a922250bb27942afbfc51d69ac1eee9397b954
lubusax <lu.bu.sax@gmail.com>
2019-05-25 20:54:36 +0200

    mechanical switch to trigger ap configuration

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,9 @@
 char mqtt_server[40];
 char mqtt_port[6] = "8080";
 char blynk_token[34] = "YOUR_BLYNK_TOKEN";
+char deepsleep_duration[3]; //in minutes
 bool shouldSaveConfig = false; //flag for saving data
+char autocon[5]="fals";
 
 #include "individual_header.h"
 
@@ -44,7 +46,7 @@ void SerialPrint_Measurement_DHT2(float temp2, float hum2);
 void MQTT_publish1(float value1, float value2);
 void MQTT_publish2(float value1, float value2);
 void WiFi_connect();
-void SerialPrint_going_to_DeepSleep();
+void SerialPrint_going_to_DeepSleep(String dur);
 
 void saveConfigCallback () {  //callback notifying us of the need to save config
   Serial.println("Should save config");
@@ -64,85 +66,119 @@ void setup() {
 void loop() {
 
   Serial.begin(9600);
-  if (WiFi.SSID()) {
-      Serial.println("Using last saved values, should be faster");
-      //trying to fix connection in progress hanging
-      ETS_UART_INTR_DISABLE();
-      wifi_station_disconnect();
-      ETS_UART_INTR_ENABLE();
 
-      WiFi.begin();
-  } else {
-      Serial.println("No saved credentials");
-    //read configuration from FS json
-    Serial.println("mounting FS...");
-
-    if (SPIFFS.begin()) {
-      Serial.println("mounted file system");
-      if (SPIFFS.exists("/config.json")) {
-        //file exists, reading and loading
-        Serial.println("reading config file");
-        File configFile = SPIFFS.open("/config.json", "r");
-        if (configFile) {
-          Serial.println("opened config file");
-          size_t size = configFile.size();
-          // Allocate a buffer to store contents of the file.
-          std::unique_ptr<char[]> buf(new char[size]);
-
-          configFile.readBytes(buf.get(), size);
-          DynamicJsonBuffer jsonBuffer;
-          JsonObject& json = jsonBuffer.parseObject(buf.get());
-          json.printTo(Serial);
-          if (json.success()) {
-            Serial.println("\nparsed json");
-
-            strcpy(mqtt_server, json["mqtt_server"]);
-            strcpy(mqtt_port, json["mqtt_port"]);
-            strcpy(blynk_token, json["blynk_token"]);
-
-          } else {
-            Serial.println("failed to load json config");
-          }
-          configFile.close();
-        }
-      }
-    } else {
-      Serial.println("failed to mount FS");
+  /*if (WiFi.getAutoConnect()) {
+    uint8_t retries = 6;
+    WiFi.reconnect();
+    while ((WiFi.status() != WL_CONNECTED) && (retries != 0));
+    {
+    retries--;
+    delay(500);
+    Serial.print(".");
     }
+  }*/
+
+
+
+  /*WiFi.reconnect();
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }*/
+
+  /*if (WiFi.SSID()) {
+    Serial.println("Using last saved values, should be faster");
+    //trying to fix connection in progress hanging
+    //ETS_UART_INTR_DISABLE();
+    //wifi_station_disconnect();
+    //ETS_UART_INTR_ENABLE();
+    WiFi.begin();
+    while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    }
+  } else {
+    Serial.println("No saved credentials");*/
+  //read configuration from FS json
+
+
+  Serial.println("mounting FS...");
+
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          Serial.println("\nparsed json");
+
+          strcpy(mqtt_server, json["mqtt_server"]);
+          strcpy(mqtt_port, json["mqtt_port"]);
+          strcpy(blynk_token, json["blynk_token"]);
+          strcpy(deepsleep_duration, json["deepsleep_duration"]);
+
+        } else {
+          Serial.println("failed to load json config");
+        }
+        configFile.close();
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
   //end read
 
 
-    // The extra parameters to be configured (can be either global or just in the setup)
-    // After connecting, parameter.getValue() will get you the configured value
-    // id/name placeholder/prompt default length
-    WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
-    WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 6);
-    WiFiManagerParameter custom_blynk_token("blynk", "blynk token", blynk_token, 32);
+  // The extra parameters to be configured (can be either global or just in the setup)
+  // After connecting, parameter.getValue() will get you the configured value
+  // id/name placeholder/prompt default length
+  WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
+  WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 6);
+  WiFiManagerParameter custom_blynk_token("blynk", "blynk token", blynk_token, 32);
+  WiFiManagerParameter custom_ds_duration("duration", "deep sleep duration (min)", deepsleep_duration, 3);
 
-    //WiFiManager
-    //Local intialization. Once its business is done, there is no need to keep it around
-    WiFiManager wifiManager;
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
 
-    //set config save notify callback
-    wifiManager.setSaveConfigCallback(saveConfigCallback);
-    wifiManager.setAPCallback(configModeCallback);
+  //set config save notify callback
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+  wifiManager.setAPCallback(configModeCallback);
 
-    //set static ip
-    //wifiManager.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
-    
-    //add all your parameters here
-    wifiManager.addParameter(&custom_mqtt_server);
-    wifiManager.addParameter(&custom_mqtt_port);
-    wifiManager.addParameter(&custom_blynk_token);
+  //set static ip
+  //wifiManager.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+  
+  //add all your parameters here
+  wifiManager.addParameter(&custom_mqtt_server);
+  wifiManager.addParameter(&custom_mqtt_port);
+  wifiManager.addParameter(&custom_blynk_token);
+  wifiManager.addParameter(&custom_ds_duration);
+  
+  if (!Is_switch_ON()) {
+    Serial.println("disconnecting");
 
-    //WiFi.disconnect(); //erases stored SSID and Password
+    WiFi.disconnect(true); //erases stored SSID and Password
+    wifiManager.startConfigPortal();
+  }
 
-    //sets timeout until configuration portal gets turned off
-    //useful to make it all retry or go to sleep
-    //in seconds
-    if (Is_switch_ON()) {
-      wifiManager.setTimeout(1); // if switch is On, then no time for WiFiManager AP portal
-    }
+  //sets timeout until configuration portal gets turned off
+  //useful to make it all retry or go to sleep
+  //in seconds
+  if (Is_switch_ON()) {
+    wifiManager.setTimeout(1); // if switch is On, then no time for WiFiManager AP portal
+  }
 
   // LED On
 
@@ -156,11 +192,12 @@ void loop() {
 
     //LED off
     //read updated parameters
-    strcpy(mqtt_server, custom_mqtt_server.getValue());
-    strcpy(mqtt_port, custom_mqtt_port.getValue());
-    strcpy(blynk_token, custom_blynk_token.getValue());
+  strcpy(mqtt_server, custom_mqtt_server.getValue());
+  strcpy(mqtt_port, custom_mqtt_port.getValue());
+  strcpy(blynk_token, custom_blynk_token.getValue());
+  strcpy(deepsleep_duration, custom_ds_duration.getValue());             
   
-  }
+  
 
   if (WiFi.status() == WL_CONNECTED)   {
     //if you get here you have connected to the WiFi
@@ -174,6 +211,7 @@ void loop() {
       json["mqtt_server"] = mqtt_server;
       json["mqtt_port"] = mqtt_port;
       json["blynk_token"] = blynk_token;
+      json["deepsleep_duration"] = deepsleep_duration;
 
       File configFile = SPIFFS.open("/config.json", "w");
       if (!configFile) {
@@ -220,9 +258,11 @@ void loop() {
   
   delay(1); // in ms
 
-  SerialPrint_going_to_DeepSleep();
-  
-  ESP.deepSleep(360000000); // in microseconds
+  SerialPrint_going_to_DeepSleep(deepsleep_duration);
+
+  int duration = atoi(deepsleep_duration)*60000000;
+
+  ESP.deepSleep(duration); // in microseconds
   
   yield(); // pass control back to background processes to prepate sleep
 }
@@ -358,8 +398,10 @@ void MQTT_publish2(float value1,float value2){
     }  
 }
 
-void SerialPrint_going_to_DeepSleep() {
-  Serial.println("Going into deep sleep for 6 minutes");
+void SerialPrint_going_to_DeepSleep(String dur) {
+  Serial.print("Going into deep sleep for ");
+  Serial.print(dur);
+  Serial.println(" minutes");
   Serial.println();
   Serial.println();
   Serial.println("************************************************");
